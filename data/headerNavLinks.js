09dc01bdfdea2c1320c92a958612ed934386b883
Devin MacGillivray <devhmac@gmail.com>
2022-12-07 11:12:58 -0700

    adding favicons, portfolio link, and removing projects from nav

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,10 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://www.devhmac.com/', title: 'Portfolio' }
+
 ]
 
 export default headerNavLinks
