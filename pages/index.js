a905a59e911b3dca74c8734dec7b8e2281773d16
Devin MacGillivray <devhmac@gmail.com>
2022-12-07 17:36:00 -0700

    comment out newsletter

--- pages/index.js
+++ pages/index.js
@@ -92,11 +92,12 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider !== '' && (
+      {/* comment this back in for newsletter access */}
+      {/* {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
