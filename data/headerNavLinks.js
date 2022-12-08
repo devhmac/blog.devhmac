c05072e4191dc4e279b97bee66f922cf2756ceca
Devin MacGillivray <devhmac@gmail.com>
2022-12-08 12:22:27 -0700

    adjust nav order

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,8 +2,8 @@ const headerNavLinks = [
   { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
   // { href: '/projects', title: 'Projects' },
-  { href: 'https://www.devhmac.com/', title: 'Portfolio' },
   { href: '/tags', title: 'Tags' },
+  { href: 'https://www.devhmac.com/', title: 'Portfolio' },
 ]
 
 export default headerNavLinks
